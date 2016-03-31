ec01501381455ce6f9c5ae4171cdb992819e1322
Mariusz <19mariusz95@gmail.com>
2016-03-31 11:29:41 +0200

    EnergyImpl updates and refactoring

--- BinaryImage/src/main/java/energy/EnergyImpl.java
+++ BinaryImage/src/main/java/energy/EnergyImpl.java
@@ -11,20 +11,64 @@ public enum EnergyImpl implements Energy {
 
     OPTION1 {
         @Override
-        public double getEnergy(boolean[][] tab, int n, NeighbourhoodStrategy strategy) {
-            double result = 0.0;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    Neighbourhood neighbourhood = new Neighbourhood(tab, n, i, j, strategy);
-                    for (Neighbour nn : neighbourhood.getNeighbourhood()) {
-                        if (Math.abs(i - nn.getRow()) < 2 && Math.abs(j - nn.getColumn()) < 2 && tab[i][j] == tab[nn.getRow()][nn.getColumn()]) {
-                            result -= 2.8 * Math.sqrt(Math.pow(Math.abs(n / 2 - nn.getRow()), 2) + Math.pow(Math.abs(n / 2 - nn.getColumn()), 2));
-                        } else
-                            result += 1.3;
-                    }
-                }
+        double getResult(boolean[][] tab, int n, NeighbourhoodStrategy strategy, double result, int i, int j) {
+            Neighbourhood neighbourhood = new Neighbourhood(tab, n, i, j, strategy);
+            for (Neighbour nn : neighbourhood.getNeighbourhood()) {
+                if (Math.abs(i - nn.getRow()) < 2 && Math.abs(j - nn.getColumn()) < 2 && tab[i][j] == tab[nn.getRow()][nn.getColumn()]) {
+                    result -= 2.8 * Math.sqrt(Math.pow(Math.abs(n / 2 - nn.getRow()), 2) + Math.pow(Math.abs(n / 2 - nn.getColumn()), 2));
+                } else
+                    result += 1.3;
+            }
+            return result;
+        }
+    }, OPTION2 {
+        @Override
+        double getResult(boolean[][] tab, int n, NeighbourhoodStrategy strategy, double result, int i, int j) {
+            Neighbourhood neighbourhood = new Neighbourhood(tab, n, i, j, strategy);
+            for (Neighbour nn : neighbourhood.getNeighbourhood()) {
+                if (Math.abs(i - nn.getRow()) < 2 && Math.abs(j - nn.getColumn()) < 2 && tab[i][j] == tab[nn.getRow()][nn.getColumn()]) {
+                    result -= 2.8;
+                } else
+                    result += 1.3;
+            }
+            return result;
+        }
+    }, OPTION3 {
+        @Override
+        double getResult(boolean[][] tab, int n, NeighbourhoodStrategy strategy, double result, int i, int j) {
+            Neighbourhood neighbourhood = new Neighbourhood(tab, n, i, j, strategy);
+            for (Neighbour nn : neighbourhood.getNeighbourhood()) {
+                if (Math.abs(i - nn.getRow()) < 2 && Math.abs(j - nn.getColumn()) < 2 && tab[i][j] == tab[nn.getRow()][nn.getColumn()]) {
+                    result += 2.8 * Math.sqrt(Math.pow(Math.abs(n / 2 - nn.getRow()), 2) + Math.pow(Math.abs(n / 2 - nn.getColumn()), 2));
+                } else
+                    result -= 1.3;
+            }
+            return result;
+        }
+
+    }, OPTION4 {
+        double getResult(boolean[][] tab, int n, NeighbourhoodStrategy strategy, double result, int i, int j) {
+            Neighbourhood neighbourhood = new Neighbourhood(tab, n, i, j, strategy);
+            for (Neighbour nn : neighbourhood.getNeighbourhood()) {
+                if (nn.getColumn() != j && nn.getRow() != i) {
+                    result -= 2.4;
+                } else
+                    result += 1.3;
             }
             return result;
         }
     };
+
+    @Override
+    public double getEnergy(boolean[][] tab, int n, NeighbourhoodStrategy strategy) {
+        double result = 0.0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result = getResult(tab, n, strategy, result, i, j);
+            }
+        }
+        return result;
+    }
+
+    abstract double getResult(boolean[][] tab, int n, NeighbourhoodStrategy strategy, double result, int i, int j);
 }
