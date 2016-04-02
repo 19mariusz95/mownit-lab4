fcc844b58d0e128e8b8954431c5205422db18578
Mariusz <19mariusz95@gmail.com>
2016-04-02 11:11:40 +0200

    ..

--- Sudoku/src/main/java/annealing/SimulatedAnnealing.java
+++ Sudoku/src/main/java/annealing/SimulatedAnnealing.java
@@ -93,7 +93,6 @@ public class SimulatedAnnealing {
             if (delta < 0 || random.nextDouble() < probability.getProbability(temp, delta)) {
                 tab = tab2;
                 applyToLabels(tab2);
-                //frame.repaint();
                 en1 = en2;
                 if (en1 == 0) {
                     System.out.println("Solved");
